f410147075839266b25a91c6ea3b5e20efe98da7
Gabriel Andrade <gaamaralpessoal@gmail.com>
2024-08-08 18:19:19 -0300

    Incluido prisma orm e postgress- finalizando o registro e auth do usuário

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
 import { SearchTestController } from "./controller/SearchTestController";
+import { AuthUserController } from "./controller/user/AuthUserController";
+import { CreatedUserController } from "./controller/user/CreatedUserController";
 
 const router = Router();
 
 //-- ROUTE TEST --
 router.get("/test", SearchTestController.handle);
+
+//-- ROUTER USER --
+router.post("/register", CreatedUserController.handle);
+router.post("/auth", AuthUserController.handle);
 
 export { router };
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,19 +1,25 @@
 import cors from "cors";
+import { config } from "dotenv";
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import { router } from "./routes";
 
+config();
+
 const app = express();
+
+// Usar variáveis de ambiente com dotenv
 const port = process.env.PORT || 3333;
+
 app.use(express.json());
 app.use(cors());
 
 app.use(router);
 
-//Middleware
+// Middleware
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof Error) {
-    //Se for uma instancia do tipo error
+    // Se for uma instância do tipo Error
     return res.status(400).json({
       error: err.message,
     });
@@ -24,6 +30,4 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   });
 });
 
-app.listen(port, () =>
-  console.log("Server running on port..." + port + "!!!!!!")
-);
+app.listen(port, () => console.log(`Server running on port ${port}...`));
